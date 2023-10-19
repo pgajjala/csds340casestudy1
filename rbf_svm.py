1866928eba003397825c6334aebc68b5c6e58c89
Jake Sanghavi <57878447+jakesanghavi@users.noreply.github.com>
2023-10-19 13:07:12 -0400

    Update rbf_svm.py
    Work with numpy instead of pandas

--- rbf_svm.py
+++ rbf_svm.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import roc_auc_score, roc_curve
@@ -7,20 +6,30 @@ from sklearn.impute import SimpleImputer
 import numpy as np
 
 
+def tprAtFPR(labels, outputs, desiredFPR):
+    fpr, tpr, thres = roc_curve(labels, outputs)
+    # True positive rate for highest false positive rate < 0.01
+    maxFprIndex = np.where(fpr <= desiredFPR)[0][-1]
+    fprBelow = fpr[maxFprIndex]
+    fprAbove = fpr[maxFprIndex + 1]
+    # Find TPR at exactly desired FPR by linear interpolation
+    tprBelow = tpr[maxFprIndex]
+    tprAbove = tpr[maxFprIndex + 1]
+    tprAt = ((tprAbove - tprBelow) / (fprAbove - fprBelow) * (desiredFPR - fprBelow)
+             + tprBelow)
+    return tprAt, fpr, tpr
+
+
 def predictTest(trainFeatures, trainLabels, testFeatures):
     # Select the best features as determined by SBS
-    # best_features = [True, True, True, True, True, True, True, True, True, True, True, True, False, True, True, True,
-    #                  True, False, True, True, True, True, True, True, True, True, True, True, True, True]
-    best_features = [True, False, False,  True,  True, False, True,  True,  True , True , True ,False,
- False ,False ,False ,False , True ,False,  True,  True,  True ,False , True, False,
- False , True  ,True  ,True , True , True]
+    best_features = [True, True, True, True, True, True, True, True, True, True, True, True, False, True, True, True,
+                     True, False, True, True, True, True, True, True, True, True, True, True, True, True]
 
-    trainFeatures = trainFeatures.loc[:, best_features]
-    testFeatures = testFeatures.loc[:, best_features]
+    trainFeatures = trainFeatures[:, best_features]
+    testFeatures = testFeatures[:, best_features]
 
     # Make a pipeline using mean imputation
-    model = make_pipeline(SimpleImputer(missing_values=-1, strategy='mean'), SVC(kernel='rbf', C=1, gamma='scale'
-                                                                                 , probability=True, class_weight='balanced'))
+    model = make_pipeline(SimpleImputer(missing_values=-1, strategy='mean'), SVC(kernel='rbf', C=10, probability=True))
 
     # Fit the model and get the predicted probabilities
     model.fit(trainFeatures, trainLabels)
@@ -31,19 +40,17 @@ def predictTest(trainFeatures, trainLabels, testFeatures):
 
 if __name__ == '__main__':
     # Read the data
-    df = pd.read_csv("spamTrain1.csv", header=None)
+    df = np.loadtxt("spamTrain1.csv", delimiter=',')
 
     # Split features and label
-    X, y = df.drop(columns=[30]), df[30]
+    X, y = df[:, :30], df[:, 30]
 
     # Train-test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=729, stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=729)
 
     # Predict the labels and get the ROC AUC score
     out = predictTest(X_train, y_train, X_test)
     score = roc_auc_score(y_test, out)
-  
-    # Best ROC AUC: 0.9098
     print(f"ROC AUC Score: {score}")
 
     # Get the full ROC curve
@@ -56,5 +63,5 @@ if __name__ == '__main__':
     # Get the TPR at the found index
     specific_tpr = tpr[idx]
 
-    # Best TPR at 1% FPR: 0.497
     print(f"True Positive Rate at {desired_fpr * 100:.2f}% False Positive Rate: {specific_tpr * 100:.2f}%")
+    print(tprAtFPR(y_test, out, 0.01)[0])
